18ae9474fdc4a99ac41122b7c3e4d6eff95b8b3d
AbdulMoizChishti <moaiz599@gmail.com>
2021-10-06 00:59:45 +0500

    Get Set Method Practice

---
+++ src/gsloop/Counting.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsloop;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Counting {
+  
+     int start, end;
+   public void getmeth(int s,int e){
+    start=s;
+    end=e;
+    for(int i=start; i<=end;i++){
+       
+         
+        if(i == 1000){
+          
+        continue;
+        
+        
+        }
+        
+        System.out.println(i);  
+    }
+       
+       }
+}
